ffb20070659d7b0e220b316065089f4a6b5ca324
zlayabarbosina <nikita.zaharuzhenkov@yandex.ru>
2025-09-28 00:25:49 +0300

    fix flake8

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
